5f541e4ffab551c6d1794b4b368cd051cbe62f2c
Mary Aguilar <johnmarkaguilar405@gmail.com>
2023-11-08 20:27:39 +0800

    inital commit

---
+++ app/src/main/java/com/example/renta/login.kt
@@ -0,0 +1,13 @@
+package com.example.renta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+
+}
